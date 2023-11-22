8e1d7e416d3dc1cd42adee230382fb413260e327
ligengying <ligengying@cmcm.com>
2023-11-22 11:35:04 +0800

    1

---
+++ script/rrspvip1.js
@@ -0,0 +1,57 @@
+var obj = JSON.parse($response.body);
+
+obj.data.sortedItems = [
+    {
+        "qualityDescription": "高清",
+        "qualityCode": "SD",
+        "canPlay": true,
+        "initialQuality": false,
+        "canShowLogin": false,
+        "canShowVip": false
+    },
+    {
+        "qualityDescription": "超清",
+        "qualityCode": "HD",
+        "canPlay": true,
+        "initialQuality": false,
+        "canShowLogin": false,
+        "canShowVip": false
+    },
+    {
+        "qualityDescription": "AI原画",
+        "qualityCode": "AI_OD",
+        "canPlay": true,
+        "initialQuality": true,
+        "canShowLogin": false,
+        "canShowVip": true
+    }
+],
+    obj.data.watchInfo.m3u8.currentQuality = "AI_OD",
+    obj.data.watchInfo.m3u8.sortedItems=[
+        {
+            "qualityDescription": "高清",
+            "qualityCode": "SD",
+            "canPlay": true,
+            "initialQuality": false,
+            "canShowLogin": false,
+            "canShowVip": false
+        },
+        {
+            "qualityDescription": "超清",
+            "qualityCode": "HD",
+            "canPlay": true,
+            "initialQuality": false,
+            "canShowLogin": false,
+            "canShowVip": false
+        },
+        {
+            "qualityDescription": "AI原画",
+            "qualityCode": "AI_OD",
+            "canPlay": true,
+            "initialQuality": true,
+            "canShowLogin": false,
+            "canShowVip": true
+        }
+    ],
+
+    $done({body: JSON.stringify(obj)});
