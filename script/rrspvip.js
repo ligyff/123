29da8661b3cc445ea4fea64490e2c4a7fdfa7334
ligengying <ligengying@cmcm.com>
2023-11-22 11:15:19 +0800

    1

--- script/rrspvip.js
+++ script/rrspvip.js
@@ -3,7 +3,7 @@ var obj = JSON.parse($response.body);
     obj.data.user.vipInfo = {
         "level": "5",
         "valid": true,
-        "expiredTime": "2099-05-26"
+        "expiredTime": "2099-05-26T05:05:04Z"
     },
     obj.data.user.vipLevel ="5"
 
