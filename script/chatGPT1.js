a790908365fc6a7f8e4c3917a7e66e939e2126db
ligengying <ligengying@cmcm.com>
2023-11-22 19:10:58 +0800

    1

---
+++ script/chatGPT1.js
@@ -0,0 +1,11 @@
+var obj = JSON.parse($response.body);
+
+
+obj.breeze = true;
+obj.dalle = true;
+obj.code_interpreter = true;
+obj.plugins = true;
+obj.workspace_gpt_custom_actions = true;
+obj.sunshine = true;
+
+$done({body: JSON.stringify(obj)});
