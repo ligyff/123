e93e41c2bf753c966eb8ae5dc42b878aa3c5dfc7
ligengying <49578664+ligengying@users.noreply.github.com>
2023-12-13 19:01:32 +0800

    Update cpp.js

--- script/cpp.js
+++ script/cpp.js
@@ -28,12 +28,12 @@ var objc = JSON.parse($response.body);
 
     objc = {
   "request_date_ms" : 1702464168646,
-  "request_date" : "2024-12-13T10:42:48Z",
+  "request_date" : "2023-12-13T10:42:48Z",
   "subscriber" : {
     "non_subscriptions" : {
 
     },
-    "first_seen" : "2024-12-13T06:00:52Z",
+    "first_seen" : "2023-12-13T06:00:52Z",
     "original_application_version" : "1",
     "other_purchases" : {
 
@@ -41,15 +41,15 @@ var objc = JSON.parse($response.body);
     "management_url" : "https://apps.apple.com/account/subscriptions",
     "subscriptions" : {
       "eticket_with_watch_6m_3d0" : {
-        "original_purchase_date" : "2024-12-13T10:33:18Z",
-        "expires_date" : "2024-12-16T10:33:17Z",
+        "original_purchase_date" : "2023-12-13T10:33:18Z",
+        "expires_date" : "2023-12-16T10:33:17Z",
         "is_sandbox" : false,
         "refunded_at" : null,
         "store_transaction_id" : "520001547916370",
         "unsubscribe_detected_at" : null,
         "grace_period_expires_date" : null,
         "period_type" : "trial",
-        "purchase_date" : "2024-12-13T10:33:17Z",
+        "purchase_date" : "2023-12-13T10:33:17Z",
         "billing_issues_detected_at" : null,
         "ownership_type" : "PURCHASED",
         "store" : "app_store",
@@ -59,9 +59,9 @@ var objc = JSON.parse($response.body);
     "entitlements" : {
       "vip+watch_vip" : {
         "grace_period_expires_date" : null,
-        "purchase_date" : "2024-12-13T10:33:17Z",
+        "purchase_date" : "2023-12-13T10:33:17Z",
         "product_identifier" : "eticket_with_watch_6m_3d0",
-        "expires_date" : "2024-12-16T10:33:17Z"
+        "expires_date" : "2023-12-16T10:33:17Z"
       }
     },
     "original_purchase_date" : "2023-12-06T10:28:48Z",
@@ -69,6 +69,7 @@ var objc = JSON.parse($response.body);
     "last_seen" : "2023-12-13T06:00:52Z"
   }
 }
+
 
 
 $done({body : JSON.stringify(objc)});
