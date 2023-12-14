e6020cb815772197238d8ef488cd8748927fd343
ligengying <49578664+ligengying@users.noreply.github.com>
2023-12-14 11:00:03 +0800

    Update cpp.js

--- script/cpp.js
+++ script/cpp.js
@@ -30,6 +30,26 @@ var currentUtcTime = objc.request_date;
 
 var timestamp =objc.request_date_ms;
 
+// 创建一个表示今天日期的 Date 对象
+var today = new Date();
+var year = today.getFullYear();
+var month = String(today.getMonth() + 1).padStart(2, '0');
+var day = String(today.getDate()).padStart(2, '0');
+// 构造新的日期字符串
+var today1 = year + '-' + month + '-' + day;
+
+// 将日期增加3天
+today.setDate(today.getDate() + 3);
+// 获取增加3天后的年、月、日
+var year1 = today.getFullYear();
+var month1 = String(today.getMonth() + 1).padStart(2, '0');
+var day1 = String(today.getDate()).padStart(2, '0');
+
+// 构造新的日期字符串
+var threeDaysLater1 = year1 + '-' + month1 + '-' + day1;
+
+
+
     objc = {
   "request_date_ms" : 1702464168646,
   "request_date" : "2023-12-13T10:42:48Z",
@@ -44,15 +64,15 @@ var timestamp =objc.request_date_ms;
     },
     "management_url" : "https://apps.apple.com/account/subscriptions",
     "subscriptions" : {
-      "eticket_with_watch_life_a" : {
+      "eticket_with_watch_6m_3d0" : {
         "original_purchase_date" : "2023-12-13T10:33:18Z",
-        "expires_date" : "9999-12-17T10:33:17Z",
+        "expires_date" : "2023-12-16T10:33:17Z",
         "is_sandbox" : false,
         "refunded_at" : null,
         "store_transaction_id" : "520001547916370",
         "unsubscribe_detected_at" : null,
         "grace_period_expires_date" : null,
-        "period_type" : "normal",
+        "period_type" : "trial",
         "purchase_date" : "2023-12-13T10:33:17Z",
         "billing_issues_detected_at" : null,
         "ownership_type" : "PURCHASED",
@@ -64,8 +84,8 @@ var timestamp =objc.request_date_ms;
       "vip+watch_vip" : {
         "grace_period_expires_date" : null,
         "purchase_date" : "2023-12-13T10:33:17Z",
-        "product_identifier" : "eticket_with_watch_life_a",
-        "expires_date" : "9999-12-17T10:33:17Z"
+        "product_identifier" : "eticket_with_watch_6m_3d0",
+        "expires_date" : "2023-12-16T10:33:17Z"
       }
     },
     "original_purchase_date" : "2023-12-06T10:28:48Z",
@@ -75,8 +95,10 @@ var timestamp =objc.request_date_ms;
 }
 
 objc.request_date=currentUtcTime;
-objc.request_date_ms =timestamp ;
+objc.request_date_ms =timestamp;
+obj.replace('2023-12-13', today1).replace('2023-12-16',threeDaysLater1)
 
+var strJson = JSON.stringify(objc);
 
 
 $done({body : JSON.stringify(objc)});
