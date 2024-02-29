775e0cbfcc9190df3b530d7adfeec1abc01647f3
ligengying <ligengying@cmcm.com>
2024-02-29 19:13:14 +0800

    1

--- script/revenuecat.js
+++ script/revenuecat.js
@@ -1,14 +1,57 @@
-var obj = JSON.parse($response.body);
+var res_new = {};
+// var res_old = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
 var headers = $request.headers;
 var userAgent = headers['User-Agent'];
 var bundleId = headers['X-Client-Bundle-ID'];
 console.log("revenuecat-------------------------" + userAgent)
 console.log("revenuecat-------------------------" + bundleId)
-if (userAgent.indexOf('Grow')) {
+
+if (typeof $response == "undefined") {
+    delete $request.headers["x-revenuecat-etag"];
+    delete $request.headers["X-RevenueCat-ETag"];
+
+    res_new.headers = $request.headers;
+} else if (bundleId === 'com.ideation.Stress') {
+    console.log("StressWatch  userAgent:" + userAgent);
+    console.log("StressWatch  bundleId:" + bundleId);
+    res_new = {
+        "request_date_ms": 1704970859633,
+        "request_date": "2024-01-11T11:00:59Z",
+        "subscriber": {
+            "non_subscriptions": {},
+            "first_seen": "2024-01-07T15:15:50Z",
+            "original_application_version": null,
+            "other_purchases": {},
+            "management_url": null,
+            "subscriptions": {
+                "stress_membership_lifetime": {
+                    "store": "app_store",
+                    "purchase_date": "2023-02-23T02:33:33Z",
+                    "ownership_type": "PURCHASED",
+                    "original_purchase_date": "2023-02-23T02:33:33Z",
+                    "expires_date": "9999-09-09T09:09:09Z"
+                }
+            },
+            "entitlements": {
+                "StressWatch Pro": {
+                    "store": "app_store",
+                    "purchase_date": "2023-02-23T02:33:33Z",
+                    "ownership_type": "PURCHASED",
+                    "original_purchase_date": "2023-02-23T02:33:33Z",
+                    "product_identifier": "stress_membership_lifetime",
+                    "expires_date": "9999-09-09T09:09:09Z"
+                }
+            },
+            "original_purchase_date": null,
+            "original_app_user_id": "$RCAnonymousID:fe1a1f5d1a4442cc9a7efecd899fbe76",
+            "last_seen": "2024-01-11T10:59:27Z"
+        }
+    }
+} else if (userAgent.indexOf('Grow')) {
     console.log("Grow  userAgent:" + userAgent);
     console.log("Grow  bundleId:" + bundleId);
-    obj = {
+    res_new = {
         "request_date_ms": 1662599120248,
         "request_date": "2022-09-08T01:05:20Z",
         "subscriber": {
@@ -44,44 +87,6 @@ if (userAgent.indexOf('Grow')) {
             "last_seen": "2022-09-08T01:04:03Z"
         }
     }
-} else if (bundleId === 'com.ideation.Stress') {
-    console.log("StressWatch  userAgent:" + userAgent);
-    console.log("StressWatch  bundleId:" + bundleId);
-    obj = {
-        "request_date_ms": 1704970859633,
-        "request_date": "2024-01-11T11:00:59Z",
-        "subscriber": {
-            "non_subscriptions": {},
-            "first_seen": "2024-01-07T15:15:50Z",
-            "original_application_version": null,
-            "other_purchases": {},
-            "management_url": null,
-            "subscriptions": {
-                "stress_membership_lifetime": {
-                    "store": "app_store",
-                    "purchase_date": "2023-02-23T02:33:33Z",
-                    "ownership_type": "PURCHASED",
-                    "original_purchase_date": "2023-02-23T02:33:33Z",
-                    "expires_date": "9999-09-09T09:09:09Z"
-                }
-            },
-            "entitlements": {
-                "StressWatch Pro": {
-                    "store": "app_store",
-                    "purchase_date": "2023-02-23T02:33:33Z",
-                    "ownership_type": "PURCHASED",
-                    "original_purchase_date": "2023-02-23T02:33:33Z",
-                    "product_identifier": "stress_membership_lifetime",
-                    "expires_date": "9999-09-09T09:09:09Z"
-                }
-            },
-            "original_purchase_date": null,
-            "original_app_user_id": "$RCAnonymousID:fe1a1f5d1a4442cc9a7efecd899fbe76",
-            "last_seen": "2024-01-11T10:59:27Z"
-        }
-    }
-
 }
 
-
-$done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(res_new)});
